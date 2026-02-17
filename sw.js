0b92716d7b4cc323d28ab2ebd216d26206a45f1c
smpit-af <asfagamecenter@gmail.com>
2026-02-17 20:39:12 +0700

    Upload index.html

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'absensi-v1';
+const assets = [
+  '/absensi-digital/index.html',
+  '/absensi-digital/manifest.json',
+  '/absensi-digital/logo/Logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
